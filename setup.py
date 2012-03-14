3a21e049cd8c50e055f1d167e8262a5ed3c6b73b
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-03-14 18:31:09 +0100

    Add setup.py and MANIFEST

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import re
+
+from setuptools import setup
+from pypi2spec import __version__
+
+description = "Small library to read/convert from XML to INI ontology file."
+
+long_description = """
+pySemon tries to help you generate an OWL ontology using INI file as basis.
+"""
+
+download_url = "http://github.com/pypingou/pySemon-%s.tar.gz" % __version__
+
+requirements = [
+    'rdflib',
+]
+
+try:
+    import argparse
+except ImportError:
+    requirements.append('argparse')
+
+setup(
+    name='pySemon',
+    version=__version__,
+    description=description,
+    author="Pierre-Yves Chibon",
+    author_email="pingou@pingoured.fr",
+    maintainer="Pierre-Yves Chibon",
+    maintainer_email="pingou@pingoured.fr",
+    url="http://github.com/pypingou/pySemon",
+    license="GPLv3+",
+    long_description=long_description,
+    download_url=download_url,
+    packages=['pySemon'],
+    install_requires=requirements,
+    entry_points="""
+    [console_scripts]
+    pypi2spec = pySemon:main
+    """
+)
